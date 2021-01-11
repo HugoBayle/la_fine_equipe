23c58f2230c6e5fb502d3ba6501dbe5e77e079d5
Hugo Bayle <h.bayle@hotmail.fr>
2021-01-11 22:47:06 +0100

    method move_to_position is now working

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
-
   validates :name, presence: true
   validates :phone_number, presence: true, numericality: true, length: { is: 10 }
   validates :bio, presence: true
